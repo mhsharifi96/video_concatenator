56465804ac3e45470d452db67cce0ded106c28e0
mohammad hossein sharifi <mh.sh7676@gmail.com>
2021-09-25 23:21:54 +0330

    inital commit

---
+++ concatenate_clip.py
@@ -0,0 +1,20 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips,ImageClip,CompositeVideoClip
+myClip = VideoFileClip("r1.mp4")
+# myClip.resize( (460,720) ) # New resolution: (460,720)
+# myClip.resize(0.8) # width and heigth multiplied by 0.6
+# myClip.resize(width=800) # height comp
+clip3 = VideoFileClip("sample.mp4")
+logo = (ImageClip("safine.png")
+          .set_duration(10)
+          .resize(height=200) # if you need to resize...
+          .margin(right=8, top=8, opacity=0) # (optional) logo-border padding
+          .set_pos(("right","top")))
+# clip3.resize( (460,720) ) # New resolution: (460,720)
+# clip3.resize(0.8) # width and heigth multiplied by 0.6
+# clip3.resize(width=800) # height comp
+final = CompositeVideoClip([clip3, logo])
+final_clip =    _videoclips([myClip,final],method='compose')
+final_clip.write_videofile("out3.mp4")
+
+
+
